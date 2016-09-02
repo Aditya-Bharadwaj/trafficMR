4d3b629e0884552832abffda5ad780a582a8d48e
Aditya Bharadwaj <aditya21nov@gmail.com>
2016-09-02 22:39:22 +0530

    Add files via upload
    Removed useless block of commented code

--- TrafficMR.java
+++ TrafficMR.java
@@ -16,23 +16,7 @@ public class TrafficMR {
 
 	public static int globalMax = Integer.MIN_VALUE;
 	public static StringBuilder sensIDMax = new StringBuilder();
-/*	
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>
-	{	
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
-		
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
-		{
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) 
-			{
-				word.set(itr.nextToken());
-				context.write(word, one);
-			}
-		}
-	}
-*/
+
 	public static class TrafficDataMapper extends Mapper<LongWritable, Text, Text, IntWritable>
 	{
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
@@ -66,21 +50,6 @@ public class TrafficMR {
 		}
 	}
 	
-/*
-	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable>
-	{
-		private IntWritable result = new IntWritable();
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
-		{
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
-			}
-			result.set(sum);
-			context.write(key, result);
-		}
-	}
-*/
 	public static class TrafficDataReducer extends Reducer<Text, IntWritable, Text, IntWritable>
 	{
 		int vehicleSum = 0;
